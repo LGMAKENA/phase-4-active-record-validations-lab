e3bc6f357e502d965754a37dc0642b6d79369ccb
lgmakena <linetmakenag@gmail.com>
2023-08-19 12:06:31 +0300

    final comm

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait_title
+  
+    private
+  
+    def clickbait_title
+      unless title&.include?("Won't Believe") || title&.include?('Secret') || title&.match?(/Top \d+/) || title&.include?('Guess')
+        errors.add(:title, "should be clickbait-y")
+      end
+    end
+  end
+  
